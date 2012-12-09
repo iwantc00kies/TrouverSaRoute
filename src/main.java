d43daa71ac02100785fa7b8d7164e10cb348b2b8
niha <nicolas.ha.prog@gmail.com>
2012-12-09 17:49:09 +0100

    test modifications mineures

--- src/main.java
+++ src/main.java
@@ -7,16 +7,12 @@ public class main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		LinkedList<Ligne> mesLignes;
 		
 		mesLignes = csv.csvCreerLignes("./files/lignes.csv");
-		//mesLignes = csv.csvCreerLignes("/users/elo/nha/Documents/TSR/stations.csv");
 		for(Ligne ligne : mesLignes){
 			ligne.Afficher();
 		}
-		
-		
 		
 		LinkedList<Arret> mesArrets;
 		mesArrets = csv.csvCreerArrets("./files/stations.csv");
