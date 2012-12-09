967ccf32b27909721710126e289c21c85d143fc8
iwantc00kies <asiegfried.enssat@gmail.com>
2012-12-09 15:45:53 +0100

    Library needed is in lib Stations and Lignes are in files No more need to set hard paths

--- src/main.java
+++ src/main.java
@@ -10,7 +10,7 @@ public class main {
 		// TODO Auto-generated method stub
 		LinkedList<Ligne> mesLignes;
 		
-		mesLignes = csv.csvCreerLignes("/home/nha/Dropbox/ENSSAT/2A_LSI/JAVA TSR/lignes.csv");
+		mesLignes = csv.csvCreerLignes("./files/lignes.csv");
 		//mesLignes = csv.csvCreerLignes("/users/elo/nha/Documents/TSR/stations.csv");
 		for(Ligne ligne : mesLignes){
 			ligne.Afficher();
@@ -19,7 +19,7 @@ public class main {
 		
 		
 		LinkedList<Arret> mesArrets;
-		mesArrets = csv.csvCreerArrets("/home/nha/Dropbox/ENSSAT/2A_LSI/JAVA TSR/stations.csv");
+		mesArrets = csv.csvCreerArrets("./files/stations.csv");
 		for(Arret arret : mesArrets){
 			arret.Afficher();
 		}
